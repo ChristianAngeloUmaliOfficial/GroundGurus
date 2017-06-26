d7330cf12414871e4b1bb9d3231364b8c80b2560
Wanda <Wanda>
2017-06-26 19:51:44 +0800

    UPDATE: gg-fragment-lifecycle

--- app/src/main/java/learning/groundgurus/MainActivity.java
+++ app/src/main/java/learning/groundgurus/MainActivity.java
@@ -1,7 +1,7 @@
 package learning.groundgurus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.ll_container, new MainFragment())
+                    .commit();
+        }
 
     }
 }
