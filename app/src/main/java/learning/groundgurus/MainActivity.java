4ce97cc3426a4e1af29a7a9c86d6a791be4a69a4
Wanda <Wanda>
2017-06-29 23:22:43 +0800

    UPDATE: Push on Shared Prefs

--- app/src/main/java/learning/groundgurus/MainActivity.java
+++ app/src/main/java/learning/groundgurus/MainActivity.java
@@ -1,14 +1,31 @@
 package learning.groundgurus;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor sharedPreferencesEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+
+        //Saving Data
+        sharedPreferencesEditor = sharedPreferences.edit();
+        sharedPreferencesEditor
+                .putString("name", "Wanda Maximoff")
+                .commit();
+
+
+        //Getting Data
+        sharedPreferences.getString("name", ""); //Empty string is default return if data not found
 
     }
 }
