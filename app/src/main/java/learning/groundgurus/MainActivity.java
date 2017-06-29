e5cb4c5a143db76fb8425c0ebe8734e991b1a829
Wanda <Wanda>
2017-06-29 22:06:13 +0800

    UPDATE: Menu Push

--- app/src/main/java/learning/groundgurus/MainActivity.java
+++ app/src/main/java/learning/groundgurus/MainActivity.java
@@ -1,7 +1,9 @@
 package learning.groundgurus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.settings:
+
+                return true;
+
+            case R.id.help:
+
+                return true;
+
+            default:
+
+                return super.onOptionsItemSelected(item);
+
+        }
 
     }
 }
