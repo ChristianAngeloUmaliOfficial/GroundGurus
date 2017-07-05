1134cbbf874f8ec191832eb4732acea865faca3c
Wanda <Wanda>
2017-07-05 22:38:46 +0800

    UPDATE: File Storage

--- app/src/main/java/learning/groundgurus/MainActivity.java
+++ app/src/main/java/learning/groundgurus/MainActivity.java
@@ -1,9 +1,17 @@
 package learning.groundgurus;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String file_name = "filename";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +19,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    private void saveTextFile(String content) {
+
+        FileOutputStream fileOutputStream = null;
+
+        try {
+            fileOutputStream = openFileOutput(file_name, Context.MODE_PRIVATE);
+            fileOutputStream.write(content.getBytes());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private String openTextFile(){
+
+        FileInputStream fileInputStream = null;
+        String data = null;
+
+        try {
+            fileInputStream = openFileInput(file_name);
+            int bufferSize = fileInputStream.available();
+            byte buffer[] = new byte[bufferSize];
+
+            fileInputStream.read(buffer);
+
+            fileInputStream.close();
+            data = new String(buffer, "UTF-8");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+
+    }
+
 }
